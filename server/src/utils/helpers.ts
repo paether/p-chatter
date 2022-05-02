695d879c5c8b3abc10fb578cb3536da35a6be963
paether <petersocial@icloud.com>
2022-05-02 21:56:48 +0200

    typescript

--- server/src/utils/helpers.ts
+++ server/src/utils/helpers.ts
@@ -1,4 +1,4 @@
-const verifyMongoIds = (ids: Array<string>) => {
+const verifyMongoIds = (ids: Array<string>): boolean => {
   const mongoRegEx = new RegExp("^[0-9a-fA-F]{24}$");
   return ids.every((id) => {
     return typeof id === "string" && mongoRegEx.test(id);
@@ -13,16 +13,22 @@ const addSocketUser = (
   userId: string,
   socketId: string,
   socketUsers: ISocketUser[]
-) => {
+): number | boolean => {
   if (socketUsers.some((user) => user.userId === userId)) return false;
   return socketUsers.push({ userId, socketId });
 };
 
-const removeSocketUser = (socketId: string, socketUsers: ISocketUser[]) => {
+const removeSocketUser = (
+  socketId: string,
+  socketUsers: ISocketUser[]
+): ISocketUser[] => {
   return socketUsers.filter((user) => user.socketId !== socketId);
 };
 
-const getSocketUser = (userId: string, socketUsers: ISocketUser[]) => {
+const getSocketUser = (
+  userId: string,
+  socketUsers: ISocketUser[]
+): ISocketUser | undefined => {
   return socketUsers.find((user) => user.userId === userId);
 };
 
